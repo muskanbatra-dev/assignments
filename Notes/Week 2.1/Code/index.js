6778c7e2e2bf006b737b042dc8c2948f625388e1
muskanbatra-dev <95659002+muskanbatra-dev@users.noreply.github.com>
2025-09-07 11:20:10 +0530

    2.1

---
+++ Notes/Week 2.1/Code/index.js
@@ -0,0 +1,24 @@
+function square(n) {
+  return n * n;
+}
+
+function cube(n) {
+  return n * n * n;
+}
+
+function sumOfSquares() {
+  let square1 = square(a);
+  let square2 = square(b);
+}
+
+let ans = sumOfSquares(1, 2);
+console.log(ans);
+
+// lets add a sum of cube function to it
+
+function sumOfCube() {
+  let square1 = cube(a);
+  let square2 = cube(b);
+
+  return square1 + square2;
+}
---
+++ Notes/Week 2.1/Readme.md
@@ -0,0 +1,8 @@
+# CALLBACKS
+
+[![Slide 1](./Images/Slide1.png)](./Images/Slide1.png)
+[![Slide 2](./Images/Slide2.png)](./Images/Slide2.png)
+[![Slide 3](./Images/Slide3.png)](./Images/Slide3.png)
+[![Slide 4](./Images/Slide4.png)](./Images/Slide4.png)
+[![Slide 5](./Images/Slide5.png)](./Images/Slide5.png)
+[![Slide 6](./Images/Slide6.png)](./Images/Slide6.png)
